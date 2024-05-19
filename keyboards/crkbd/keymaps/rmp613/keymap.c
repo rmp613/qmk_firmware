7f1cf5ef2d4a485cc09e37c9c3e0abf7ac6d3bcb
rmp613 <rmp613@gmail.com>
2024-05-19 16:47:01 +1000

    add swpper, start on keymap, hide keyboards from filetree

--- keyboards/crkbd/keymaps/rmp613/keymap.c
+++ keyboards/crkbd/keymaps/rmp613/keymap.c
@@ -3,46 +3,75 @@
 #include "features/achordion.h"
 // #include "features/custom_shift_keys.h"
 
-// Left-hand home row mods
-#define HOME_C LCTL_T(KC_C)
-#define HOME_I LALT_T(KC_I)
-#define HOME_E LGUI_T(KC_E)
-#define HOME_A LSFT_T(KC_A)
+// Left-hand HRM row mods
+#define HRM_C LCTL_T(KC_C)
+#define HRM_I LALT_T(KC_I)
+#define HRM_E LGUI_T(KC_E)
+#define HRM_A LSFT_T(KC_A)
 
-// Right-hand home row mods
-#define HOME_H RSFT_T(KC_H)
-#define HOME_T RGUI_T(KC_T)
-#define HOME_S LALT_T(KC_S)
-#define HOME_N RCTL_T(KC_N)
+// Right-hand HRM row mods
+#define HRM_H RSFT_T(KC_H)
+#define HRM_T RGUI_T(KC_T)
+#define HRM_S LALT_T(KC_S)
+#define HRM_N RCTL_T(KC_N)
+
+enum combos {
+    COMBO_LCTL,
+    COMBO_LGUI,
+    COMBO_LALT,
+    COMBO_LCTLGUI,
+    // more here...
+
+    COMBO_LENGTH // nifty trick to avoid manually specifying how many combos you have
+};
+
+uint16_t COMBO_LEN = COMBO_LENGTH; // nifty trick continued
+
+// define keys that make up combos
+const uint16_t PROGMEM fd_combo[] = {KC_F, KC_D, COMBO_END};
+const uint16_t PROGMEM fs_combo[] = {KC_F, KC_S, COMBO_END};
+const uint16_t PROGMEM fa_combo[] = {KC_F, KC_A, COMBO_END};
+const uint16_t PROGMEM fds_combo[] = {KC_F, KC_D, KC_S, COMBO_END};
+// more here...
+
+// map combo names to their keys and the key they trigger
+combo_t key_combos[] = {
+    [COMBO_LCTL] = COMBO(fd_combo, KC_LCTL),
+    [COMBO_LGUI] = COMBO(fs_combo, KC_LGUI),
+    [COMBO_LALT] = COMBO(fa_combo, KC_LALT),
+    [COMBO_LCTLGUI] = COMBO(fds_combo, LCTL(KC_LGUI)),
+    // [EM_EMAIL] = COMBO_ACTION(email_combo),
+    // more here...
+};
 
 
-bool achordion_chord(uint16_t tap_hold_keycode,
-                     keyrecord_t* tap_hold_record,
-                     uint16_t other_keycode,
-                     keyrecord_t* other_record) {
-  // Exceptionally consider the following chords as holds, even though they
-  // are on the same hand in Dvorak.
- // switch (tap_hold_keycode) {
-  //  case HOME_A:  // A + U.
-  //    if (other_keycode == HOME_U) { return true; }
-  //    break;
+bool achordion_chord(uint16_t tap_hold_keycode, keyrecord_t* tap_hold_record, uint16_t other_keycode, keyrecord_t* other_record) {
+    // Exceptionally consider the following chords as holds, even though they
+    // are on the same hand in Dvorak.
+    // switch (tap_hold_keycode) {
+    //  case HRM_A:  // A + U.
+    //    if (other_keycode == HRM_U) { return true; }
+    //    break;
 
-  //  case HOME_S:  // S + H and S + G.
- //     if (other_keycode == HOME_H || other_keycode == KC_G) { return true; }
-  //    break;
-//  }
+    //  case HRM_S:  // S + H and S + G.
+    //     if (other_keycode == HRM_H || other_keycode == KC_G) { return true; }
+    //    break;
+    //  }
 
-  // Also allow same-hand holds when the other key is in the rows below the
-  // alphas. I need the `% (MATRIX_ROWS / 2)` because my keyboard is split.
-  if (other_record->event.key.row % (MATRIX_ROWS / 2) >= 4) { return true; }
+    // Also allow same-hand holds when the other key is in the rows below the
+    // alphas. I need the `% (MATRIX_ROWS / 2)` because my keyboard is split.
+    if (other_record->event.key.row % (MATRIX_ROWS / 2) >= 4) {
+        return true;
+    }
 
-  // Otherwise, follow the opposite hands rule.
-  return achordion_opposite_hands(tap_hold_record, other_record);
+    // Otherwise, follow the opposite hands rule.
+    return achordion_opposite_hands(tap_hold_record, other_record);
 }
 
 uint16_t achordion_timeout(uint16_t tap_hold_keycode) {
   return 800;  // Otherwise use a timeout of 800 ms.
 }
+
 bool process_record_user(uint16_t keycode, keyrecord_t* record) {
   if (!process_achordion(keycode, record)) { return false; }
 //   if (!process_custom_shift_keys(keycode, record)) { return false; }
@@ -59,39 +88,28 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
   }
   return true;
 }
+
 void matrix_scan_user(void) {
   achordion_task();
 }
-enum combo_events {
-  EM_EMAIL,
-  BSPC_LSFT_CLEAR,
-};
 
-const uint16_t PROGMEM email_combo[] = {KC_E, KC_M, COMBO_END};
-const uint16_t PROGMEM clear_line_combo[] = {KC_BSPC, KC_LSFT, COMBO_END};
+// void process_combo_event(uint16_t combo_index, bool pressed) {
+//   switch(combo_index) {
+//     case EM_EMAIL:
+//       if (pressed) {
+//       //  SEND_STRING("john.doe@example.com");
+//       }
+//       break;
+//     case BSPC_LSFT_CLEAR:
+//       if (pressed) {
+//      //   tap_code16(KC_END);
+//        // tap_code16(S(KC_HRM));
+//         //tap_code16(KC_BSPC);
+//       }
+//       break;
+//   }
+// }
 
-combo_t key_combos[] = {
-  [EM_EMAIL] = COMBO_ACTION(email_combo),
-  [BSPC_LSFT_CLEAR] = COMBO_ACTION(clear_line_combo),
-};
-/* COMBO_ACTION(x) is same as COMBO(x, KC_NO) */
-
-void process_combo_event(uint16_t combo_index, bool pressed) {
-  switch(combo_index) {
-    case EM_EMAIL:
-      if (pressed) {
-      //  SEND_STRING("john.doe@example.com");
-      }
-      break;
-    case BSPC_LSFT_CLEAR:
-      if (pressed) {
-     //   tap_code16(KC_END);
-       // tap_code16(S(KC_HOME));
-        //tap_code16(KC_BSPC);
-      }
-      break;
-  }
-}
 bool process_detected_host_os_kb(os_variant_t detected_os) {
     if (!process_detected_host_os_user(detected_os)) {
         return false;
@@ -117,9 +135,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,--------------------------------------------------------------.  ,--------------------------------------------------------------.
       XXXXXXX,    KC_B,    KC_Y,    KC_O,    KC_U, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX,    KC_L,    KC_D,    KC_W,    KC_V, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-       KC_TAB,  HOME_C,  HOME_I,  HOME_E,  HOME_A, KC_COMM, XXXXXXX,    XXXXXXX,  KC_DOT,  HOME_H,  HOME_T,  HOME_S,  HOME_N,    KC_Q,
+       XXXXXXX, HRM_C,  HRM_I,  HRM_E,  HRM_A, KC_COMM, XXXXXXX,    XXXXXXX,  KC_DOT,  HRM_H,  HRM_T,  HRM_S,  HRM_N,    KC_Q,
   //|--------+--------+--------+--------+--------+--------+--------'  `--------+--------+--------+--------+--------+--------+--------|
-      KC_LSFT,    KC_G,    KC_X,    KC_J,    KC_K,KC_MINUS,                      KC_SLSH,    KC_R,    KC_M,    KC_F,    KC_P, KC_LSFT,
+      XXXXXXX,    KC_G,    KC_X,    KC_J,    KC_K,KC_MINUS,                      KC_SLSH,    KC_R,    KC_M,    KC_F,    KC_P, KC_LSFT,
   //|--------+--------+--------+--------+--------+--------+--------.  ,--------+--------+--------+--------+--------+--------+--------|
                                           KC_LALT, TL_LOWR,  KC_SPC,   KC_RCTL, TL_UPPR, KC_RSFT
                                       //`--------------------------'  `--------------------------'
@@ -127,11 +145,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [1] = LAYOUT_split_3x6_3_ex2(
   //,--------------------------------------------------------------.  ,--------------------------------------------------------------.
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      XXXXXXX, XXXXXXX,    KC_7,    KC_8,    KC_9, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      XXXXXXX,    KC_0,    KC_4,    KC_5,    KC_6, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------'  `--------+--------+--------+--------+--------+--------+--------|
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      XXXXXXX, XXXXXXX,    KC_1,    KC_2,    KC_3, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------.  ,--------+--------+--------+--------+--------+--------+--------|
                                           _______, _______, _______,    _______, _______, _______
                                       //`--------------------------'  `--------------------------'
